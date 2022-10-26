32990a7a7b2b34a95db95accc3feb02192c15972
rsk25 <montgomery7289@gmail.com>
2022-10-26 18:22:25 +0900

    producer-consumer IPC examples

---
+++ 3.16_shm_consumer.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <sys/shm.h>
+#include <sys/stat.h>
+#include <sys/mman.h>
+
+int main()
+{
+    const int SIZE = 4096;
+    const char *name = "OS";
+    const char *message_0 = "Hello, ";
+    const char *message_1 = "Shared Memory!\n";
+
+    int shm_fd;
+    char *ptr;
+
+    shm_fd = shm_open(name, O_RDONLY, 0666);
+
+    ptr = (char *)mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    
+    printf("%s", (char *)ptr);
+    /* remove the shared memory */
+    shm_unlink(name);
+
+    return 0;
+}
